42e2d1a9353a14a36bce3e8e1eb8e1883126d44b
Daniel Martinez Maimone <96289800+ZapatoProgramming@users.noreply.github.com>
2024-11-25 22:12:12 -0600

    Update mongodbConnection.js

--- thirdLayer/mongodbConnection.js
+++ thirdLayer/mongodbConnection.js
@@ -1,5 +1,5 @@
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://danielmaimone03:_tf6MSe7mW9vapX@chichenitza.odfli.mongodb.net/?retryWrites=true&w=majority&appName=ChichenItza"; 
+const uri = ""; 
 
 // Crea un cliente MongoClient con la API estable
 const client = new MongoClient(uri, {
